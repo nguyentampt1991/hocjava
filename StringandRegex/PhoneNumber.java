2ac22a3c588c4e3eea3db1a885db38633c25f129
nguyentampt1991 <86640565+nguyentampt1991@users.noreply.github.com>
2021-07-06 09:07:57 +0700

    2

---
+++ StringandRegex/PhoneNumber.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trainning.StringandRegex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Tam Nguyen
+ */
+public class PhoneNumber {
+     private static final String ACCOUNT_REGEX = "^\\(\\d{2}\\)\\-\\(0\\d{9}\\)$";
+    private static String[] validPhoneNumber = {"(84)-(0978489648)"};
+    private static String[] inValidPhoneNumber = {"(a8)-(22222222)"};
+
+    public static void main(String[] args){
+        if(validate(validPhoneNumber[0])){
+            System.out.println(validPhoneNumber[0] + " is valid name");
+        } else {
+            System.out.println(validPhoneNumber[0] + " is not valid name");
+        }
+
+        if(validate(inValidPhoneNumber[0])){
+            System.out.println(inValidPhoneNumber[0] + " is valid name");
+        } else {
+            System.out.println(inValidPhoneNumber[0] + " is not valid name");
+        }
+    }
+
+    public static boolean validate(String regex) {
+		Pattern pattern = Pattern.compile(ACCOUNT_REGEX);
+		Matcher  matcher = pattern.matcher(regex);
+		return matcher.matches();
+	}
+}
+    
+
